f0fb002d99d8d78ac7a93e53f2322314b8e52280
DelioAraujo <delioaraujo@gmail.com>
2023-03-22 19:44:25 -0300

    itens de 2 a 6 em andamento

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,26 @@
+import {
+  adicionaElementCarregando,
+  adicionaMensagemDeErro,
+  retiraElementoCarregando,
+} from '../main';
+
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = (parametro) => {
+  if (parametro === undefined) {
+    throw new Error('Termo de busca não informado');
+  } else {
+    adicionaElementCarregando();
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`)
+      .then((response) => response.json())
+      .then((data) => {
+        retiraElementoCarregando();
+        return data.results;
+      })
+      .catch(() => {
+        adicionaMensagemDeErro();
+      });
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,50 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+//item 03---------------------------------------------------------
+//coloca o retorno da função fetchProductsList em uma constante
+const listaDeProdutos = await fetchProductsList('computador');
+// usando a constante listaDeProdutos, aplica-se um map usará cada elemento da listaDeProdutos como parâmetro na função createProductElement que criará um novo elemento no html da página.
+const listaDeProdutosFinal = listaDeProdutos.map((produto) =>
+  createProductElement({
+    id: produto.id,
+    title: produto.title,
+    thumbnail: produto.thumbnail,
+    price: produto.price,
+  });
+);
+//item 04 -----------------------------------------------
+//faz o novo elemento "carregando..."
+export const adicionaElementCarregando = () => {
+  const carrinho = document.querySelector('.cart');
+  const elementoLoading = document.createElement('p');
+  elementoLoading.classList.add('loading')
+  elementoLoading.innerHTML = 'carregando...'
+
+  carrinho.appendChild(elementoLoading)
+
+}
+
+//remove o elemento "carregando..."
+export const retiraElementoCarregando = () => {
+  const elementoLoading = document.querySelector('.cart p');
+  elementoLoading.remove();
+
+}
+
+//funções adicionadas no arquivo fetchFunctions.js, dentro da função retchProductsList
+
+//item 5--------------------------------------------------
+//faz o novo elemento "carregando..."
+export const adicionaMensagemDeErro = () => {
+  const carrinho = document.querySelector('.cart');
+  const elementoLoading = document.createElement('p');
+  elementoLoading.classList.add('erro')
+  elementoLoading.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente'
+
+  carrinho.appendChild(elementoLoading)
+
+}
