dcabc85c14bb2b0f7a1a3f496a4235f39c88b550
DelioAraujo <delioaraujo@gmail.com>
2023-03-23 09:57:20 -0300

    item 7 em andamento

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,23 +4,28 @@ import {
   retiraElementoCarregando,
 } from '../main';
 
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (id) => {
+  if (id === undefined) {
+    throw new Error('ID não informado');
+  } else {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const data = await response.json();
+    return data;
+  }
 };
 
-export const fetchProductsList = (parametro) => {
+export const fetchProductsList = async (parametro) => {
   if (parametro === undefined) {
     throw new Error('Termo de busca não informado');
   } else {
     adicionaElementCarregando();
-    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`)
-      .then((response) => response.json())
-      .then((data) => {
-        retiraElementoCarregando();
-        return data.results;
-      })
-      .catch(() => {
-        adicionaMensagemDeErro();
-      });
+    try {
+      const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`);
+      const data = await response.json();
+      retiraElementoCarregando();
+      return data.results;
+    } catch {
+      adicionaMensagemDeErro();
+    }
   }
 };
